02842e0d58edb787ef0f200ec8ded0016c8930d2
Muhammad Umair Tahir <nobelhighschool@hotmail.com>
2020-02-15 12:14:48 +0500

    added cities controller

--- EFarmer.pk/Controllers/ApiControllers/CitiesController.cs
+++ EFarmer.pk/Controllers/ApiControllers/CitiesController.cs
@@ -1,10 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using Autofac;
 using EFarmer.Models;
 using EFarmer.pk.Models;
+using EFarmer.pk.ViewModels.ApiModels;
 using EFarmerPkModelLibrary.Factories;
 using EFarmerPkModelLibrary.Repositories;
 using Microsoft.AspNetCore.Http;
@@ -21,7 +21,7 @@ namespace EFarmer.pk.Controllers.ApiControllers
     public class CitiesController : ControllerBase
     {
         private readonly IContainer container;
-        private RepositoryFactory repositoryFactory;
+        private readonly RepositoryFactory repositoryFactory;
         /// <summary>
         /// Initializer
         /// </summary>
@@ -30,6 +30,10 @@ namespace EFarmer.pk.Controllers.ApiControllers
             repositoryFactory = new ModelsFactory();
             container = repositoryFactory.Build();
         }
+        /// <summary>
+        /// Returns a list of cities
+        /// </summary>
+        /// <returns></returns>
         // GET: api/Cities
         [HttpGet]
         [ProducesResponseType(typeof(List<City>), StatusCodes.Status200OK)]
@@ -53,30 +57,122 @@ namespace EFarmer.pk.Controllers.ApiControllers
                 return BadRequest(ex.Message);
             }
         }
-
+        /// <summary>
+        /// Returns a city searched by primary key
+        /// </summary>
+        /// <param name="id">Primary Key</param>
+        /// <returns></returns>
         // GET: api/Cities/5
         [HttpGet("{id}", Name = "GetCity")]
-        public string Get(int id)
+        [ProducesResponseType(typeof(City), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public ActionResult Get(short id)
         {
-            return "value";
+            try
+            {
+                City city = null;
+                using (var scope = container.BeginLifetimeScope())
+                {
+                    using (var repository = scope.Resolve<ICityRepository>())
+                    {
+                        city = repository.Read(id);
+                    }
+                }
+                return Ok(city);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
         }
-
-        // POST: api/Cities
-        [HttpPost]
-        public void Post([FromBody] string value)
+        /// <summary>
+        /// Creates a new city
+        /// </summary>
+        /// <param name="city">Data for City</param>
+        [HttpPost("", Name = "PostCity")]
+        [ProducesResponseType(typeof(City), StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public ActionResult<City> Post([FromBody] CityApiModel city)
         {
+            try
+            {
+                City _city = new City
+                {
+                    GeoLocation = city.Location,
+                    Name = city.Name
+                };
+                using (var scope = container.BeginLifetimeScope())
+                {
+                    using (var repository = scope.Resolve<ICityRepository>())
+                    {
+                        _city = repository.Read(repository.Create(_city));
+                    }
+                }
+                return _city;
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
         }
-
-        // PUT: api/Cities/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        /// <summary>
+        /// Updates the city
+        /// </summary>
+        /// <param name="city">City to be updated</param>
+        // PUT: api/Users/5
+        [HttpPut("Update", Name = "UpdateCity")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public IActionResult Put([FromBody] CityApiModel city)
         {
+            try
+            {
+                City _city = new City
+                {
+                    GeoLocation = city.Location,
+                    Name = city.Name,
+                    Id = city.Id
+                };
+                using (var scope = container.BeginLifetimeScope())
+                {
+                    using (var repository = scope.Resolve<ICityRepository>())
+                    {
+                        repository.Update(_city);
+                    }
+                }
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
         }
-
-        // DELETE: api/ApiWithActions/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
+        /// <summary>
+        /// Deletes this city
+        /// </summary>
+        /// <param name="id">Primary Key</param>
+        // DELETE: api/Delete/5
+        [HttpDelete("Delete/{id}", Name = "DeleteCity")]
+        [ProducesResponseType(typeof(bool), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public ActionResult<bool> Delete(short id)
         {
+            try
+            {
+                bool flag = false;
+                using (var scope = container.BeginLifetimeScope())
+                {
+                    using (var repository = container.Resolve<ICityRepository>())
+                    {
+                        flag = repository.Delete(id);
+                    }
+                }
+                return flag;
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
         }
     }
 }
---
+++ EFarmer.pk/ViewModels/ApiModels/CityApiModel.cs
@@ -0,0 +1,27 @@
+﻿using EFarmer.Models.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EFarmer.pk.ViewModels.ApiModels
+{
+    /// <summary>
+    /// Api Model for City
+    /// </summary>
+    public class CityApiModel
+    {
+        /// <summary>
+        /// Name of the City
+        /// </summary>
+        public string Name { get; set; }
+        /// <summary>
+        /// Location of Existence
+        /// </summary>
+        public GeoLocation Location { get; set; }
+        /// <summary>
+        /// Primary Key
+        /// </summary>
+        public short Id { get; set; }
+    }
+}
