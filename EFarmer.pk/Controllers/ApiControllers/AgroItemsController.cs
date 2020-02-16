98857aa765007f641c46a20fe0f19974d01026a8
Muhammad Umair Tahir <nobelhighschool@hotmail.com>
2020-02-16 12:59:16 +0500

    added agroItems api controller

---
+++ EFarmer.pk/Controllers/ApiControllers/AgroItemsController.cs
@@ -0,0 +1,235 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Autofac;
+using EFarmer.Models;
+using EFarmer.pk.ApiModels;
+using EFarmer.pk.Models;
+using EFarmer.pk.ViewModels.ApiModels;
+using EFarmerPkModelLibrary.Factories;
+using EFarmerPkModelLibrary.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EFarmer.pk.Controllers.ApiControllers
+{
+    /// <summary>
+    /// Controls the requests and responses for Agro Items
+    /// </summary>
+    [Route("api/[controller]")]
+    [ApiController]
+    [Produces("application/json")]
+    public class AgroItemsController : ControllerBase
+    {
+        private readonly IContainer container;
+        private RepositoryFactory repositoryFactory;
+        /// <summary>
+        /// Initializer
+        /// </summary>
+        public AgroItemsController()
+        {
+            repositoryFactory = new ModelsFactory();
+            container = repositoryFactory.Build();
+        }
+        /// <summary>
+        /// Returns a list of agro items
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet("", Name = "GetAllItems")]
+        [ProducesResponseType(typeof(List<AgroItem>), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<ActionResult<List<AgroItem>>> Get()
+        {
+            try
+            {
+                List<AgroItem> agroItems = new List<AgroItem>();
+                using (var scope = container.BeginLifetimeScope())
+                {
+                    using (var repository = scope.Resolve<IAgroItemRepository>())
+                    {
+                        agroItems = await repository.ReadAllAsync();
+                    }
+                }
+                return Ok(agroItems);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+        /// <summary>
+        /// Returns a list of items fav by buyer
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet("FavItems/{buyerId}", Name = "GetFavItems")]
+        [ProducesResponseType(typeof(List<AgroItem>), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<ActionResult<List<AgroItem>>> GetFavItems(long buyerId)
+        {
+            try
+            {
+                List<AgroItem> agroItems = new List<AgroItem>();
+                using (var scope = container.BeginLifetimeScope())
+                {
+                    using (var repository = scope.Resolve<IAgroItemRepository>())
+                    {
+                        agroItems = await repository.GetInterestedItemsAsync(new User { Id = buyerId });
+                    }
+                }
+                return Ok(agroItems);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+        /// <summary>
+        /// Returns a list of agro items in a category
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet("ItemsByCategory/{categoryId}", Name = "GetItemsByCategory")]
+        [ProducesResponseType(typeof(List<AgroItem>), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<ActionResult<List<AgroItem>>> GetItemsByCategory(short categoryId)
+        {
+            try
+            {
+                List<AgroItem> agroItems = new List<AgroItem>();
+                using (var scope = container.BeginLifetimeScope())
+                {
+                    using (var repository = scope.Resolve<IAgroItemRepository>())
+                    {
+                        agroItems = await repository.GetAgroItemsByCategoryAsync(new Category { Id = categoryId });
+                    }
+                }
+                return Ok(agroItems);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+        /// <summary>
+        /// Returns an Agro Item
+        /// </summary>
+        /// <param name="id">Primary Key</param>
+        /// <returns></returns>
+        [HttpGet("{id}", Name = "GetAgroItem")]
+        [ProducesResponseType(typeof(AgroItem), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public ActionResult<AgroItem> Get(int id)
+        {
+            try
+            {
+                AgroItem agroItem = null;
+                using (var scope = container.BeginLifetimeScope())
+                {
+                    using (var repository = scope.Resolve<IAgroItemRepository>())
+                    {
+                        agroItem = repository.Read(id);
+                    }
+                }
+                return agroItem;
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+        /// <summary>
+        /// Creates a new Agro Item
+        /// </summary>
+        /// <param name="agroItem">Data for AgroItem</param>
+        [HttpPost("", Name = "PostAgroItem")]
+        [ProducesResponseType(typeof(AgroItem), StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public ActionResult<AgroItem> Post([FromBody] AgroItemResponseModel agroItem)
+        {
+            try
+            {
+                AgroItem _agroItem = new AgroItem
+                {
+                    Category = new Category { Id = agroItem.Category },
+                    Name = agroItem.Name,
+                    UrduName = agroItem.UrduName,
+                    UrduWeightScale = agroItem.UrduWeightScale,
+                    WeightScale = agroItem.WeightScale
+                };
+                using (var scope = container.BeginLifetimeScope())
+                {
+                    using (var repository = scope.Resolve<IAgroItemRepository>())
+                    {
+                        _agroItem = repository.Read(repository.Create(_agroItem));
+                    }
+                }
+                return _agroItem;
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// Updates the AgroItem
+        /// </summary>
+        /// <param name="agroItem">Agro Item to be updated</param>
+        [HttpPut("Update", Name = "UpdateAgroItem")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public IActionResult Put([FromBody] AgroItemResponseModel agroItem)
+        {
+            try
+            {
+                AgroItem _agroItem = new AgroItem
+                {
+                    Category = new Category { Id = agroItem.Category },
+                    Name = agroItem.Name,
+                    UrduName = agroItem.UrduName,
+                    UrduWeightScale = agroItem.UrduWeightScale,
+                    WeightScale = agroItem.WeightScale
+                };
+                using (var scope = container.BeginLifetimeScope())
+                {
+                    using (var repository = scope.Resolve<IAgroItemRepository>())
+                    {
+                        repository.Update(_agroItem);
+                    }
+                }
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// Deletes this agroItem
+        /// </summary>
+        /// <param name="id"></param>
+        [HttpDelete("Delete/{id}", Name = "DeleteAgroItem")]
+        [ProducesResponseType(typeof(bool), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public ActionResult<bool> Delete(int id)
+        {
+            try
+            {
+                bool flag = false;
+                using (var scope = container.BeginLifetimeScope())
+                {
+                    using (var repository = container.Resolve<IAgroItemRepository>())
+                    {
+                        flag = repository.Delete(id);
+                    }
+                }
+                return flag;
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
---
+++ EFarmer.pk/ViewModels/ApiModels/AgroItemResponseModel.cs
@@ -0,0 +1,33 @@
+﻿namespace EFarmer.pk.ViewModels.ApiModels
+{
+    /// <summary>
+    /// Data model to request agro item api
+    /// </summary>
+    public class AgroItemResponseModel
+    {
+        /// <summary>
+        /// Cateogry ot which it belongs
+        /// </summary>
+        public short Category { get; set; }
+        /// <summary>
+        /// Weight Scale in urdu
+        /// </summary>
+        public string UrduWeightScale { get; set; }
+        /// <summary>
+        /// Weight scale in english
+        /// </summary>
+        public string WeightScale { get; set; }
+        /// <summary>
+        /// Urdu name of the agro item
+        /// </summary>
+        public string UrduName { get; set; }
+        /// <summary>
+        /// Name of the Agro Item
+        /// </summary>
+        public string Name { get; set; }
+        /// <summary>
+        /// Primary Key
+        /// </summary>
+        public int Id { get; set; }
+    }
+}
