96daefce19c72b21471f1f31482eb324ea55aa8b
Muhammad Umair Tahir <nobelhighschool@hotmail.com>
2020-02-23 23:14:05 +0500

    added local language support for urdu

--- EFarmer.pk/Controllers/HomeController.cs
+++ EFarmer.pk/Controllers/HomeController.cs
@@ -1,13 +1,35 @@
-﻿using EFarmer.ViewModels;
+﻿using Autofac;
+using EFarmer.Models;
+using EFarmer.pk.Models;
+using EFarmer.ViewModels;
+using EFarmerPkModelLibrary.Repositories;
+using Microsoft.AspNetCore.Localization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Localization;
+using System;
 using System.Diagnostics;
 
 namespace EFarmer.pk.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        private readonly IContainer _container;
+        private readonly IStringLocalizer<HomeController> _localizer;
+        public HomeController(IStringLocalizer<HomeController> localizer)
         {
+            _container = new ModelsFactory().Build();
+            _localizer = localizer;
+        }
+        public IActionResult Index(string culture)
+        {
+            culture = (culture == null) ? "en" : culture;
+            if (Request.Cookies[CookieRequestCultureProvider.DefaultCookieName] != null
+                && !Request.Cookies[CookieRequestCultureProvider.DefaultCookieName].Contains(CookieRequestCultureProvider.MakeCookieValue(new RequestCulture(culture))))
+            {
+                Response.Cookies.Append(CookieRequestCultureProvider.DefaultCookieName,
+                        CookieRequestCultureProvider.MakeCookieValue(new RequestCulture(culture)),
+                        new Microsoft.AspNetCore.Http.CookieOptions { Expires = DateTimeOffset.UtcNow.AddMonths(1) });
+            }
             return View();
         }
 
@@ -34,6 +56,22 @@ namespace EFarmer.pk.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public async System.Threading.Tasks.Task<IActionResult> GetAgroItemsByCategory(short id)
+        {
+            string options = "<option label=" + _localizer["Select_an_Item"] + "></option>";
+            using (var scope = _container.BeginLifetimeScope())
+            {
+                using (var itemsRepository = scope.Resolve<IAgroItemRepository>())
+                {
+                    foreach (var item in await itemsRepository.GetAgroItemsByCategoryAsync(new Category { Id = id }))
+                    {
+                        options += "<option value='" + item.Id + "'>" + _localizer[item.Name].Value + "</option>";
+                    }
+                }
+            }
+            return Content(options);
         }
     }
 }
