59b8739adf2ae748e2de039371bc103040e888d4
Muhammad Umair Tahir <nobelhighschool@hotmail.com>
2020-02-29 19:14:52 +0500

    adjusted layout

---
+++ EFarmer.pk/ViewModels/AccountViewModels.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EFarmer.pk.ViewModels.AccountViewModels
+{
+    public class RegisterAccountViewModel
+    {
+        [Required]
+        [Display(Name="Full Name")]
+        public string Name { get; set; }
+        [Required]
+        [Display(Name ="Contact Number")]
+        public string ContactNumber { get; set; }
+        [Required]
+        [Display(Name ="Email")]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+        [Required]
+        [Display(Name ="Password")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
