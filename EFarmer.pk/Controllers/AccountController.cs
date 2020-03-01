9b453735a6372a2835177ef82b6df4bcd07a3622
Muhammad Umair Tahir <nobelhighschool@hotmail.com>
2020-03-01 19:07:35 +0500

    added register page

--- EFarmer.pk/Controllers/AccountController.cs
+++ EFarmer.pk/Controllers/AccountController.cs
@@ -2,13 +2,32 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using EFarmer.pk.ViewModels.AccountViewModels;
 using Microsoft.AspNetCore.Mvc;
 
 namespace EFarmer.pk.Controllers
 {
     public class AccountController : Controller
     {
-        public IActionResult Index()
+        public IActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Register(RegisterAccountViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+            return RedirectToAction("Index", "Home");
+        }
+        public IActionResult Login()
+        {
+            return View();
+        }
+        public IActionResult Terms()
         {
             return View();
         }
--- EFarmer.pk/ViewModels/AccountViewModels.cs
+++ EFarmer.pk/ViewModels/AccountViewModels.cs
@@ -5,18 +5,32 @@ namespace EFarmer.pk.ViewModels.AccountViewModels
     public class RegisterAccountViewModel
     {
         [Required]
-        [Display(Name="Full Name")]
+        [Display(Name="Full Name", Description ="Enter your full name")]
         public string Name { get; set; }
         [Required]
         [Display(Name ="Contact Number")]
+        [Phone]
         public string ContactNumber { get; set; }
-        [Required]
         [Display(Name ="Email")]
-        [DataType(DataType.EmailAddress)]
+        [Required]
+        [EmailAddress]
         public string Email { get; set; }
         [Required]
         [Display(Name ="Password")]
         [DataType(DataType.Password)]
         public string Password { get; set; }
+    }
+    public class LoginAccountViewModel
+    {
+        [Required]
+        [Display(Name = "Email")]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        [Display(Name = "Password")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        [Display(Name = "Remember Me")]
+        public bool IsRemembered { get; set; }
     }
 }
